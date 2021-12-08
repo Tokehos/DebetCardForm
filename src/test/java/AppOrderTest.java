493039beb44cb80de74238c009a3a0fdc02a53e7
tokehos <max-lukich@mail.ru>
2021-12-08 15:15:15 +0300

    update tests

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -9,12 +9,32 @@ public class AppOrderTest {
 
 
     @Test
-    void shouldDebetCerdForm() {
+    void shouldDebetCardForm() {
         open("http://localhost:9999/");
         $("[name='name']").setValue("Лиукконен Максим");
         $("[name='phone']").setValue("+79819715780");
         $("[class='checkbox__box']").click();
         $("[role='button']").click();
         $(withText("Ваша заявка успешно отправлена!")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldDebetCardFormWithInvalidNameForm() {
+        open("http://localhost:9999/");
+        $("[name='name']").setValue("Liukkonen Maksim");
+        $("[name='phone']").setValue("+79819715780");
+        $("[class='checkbox__box']").click();
+        $("[role='button']").click();
+        $(withText("Имя и Фамилия указаные неверно")).shouldBe(visible);
+    }
+
+    @Test
+    void shouldDebetCardFormWithInvalidPhoneForm() {
+        open("http://localhost:9999/");
+        $("[name='name']").setValue("Лиукконен Максим");
+        $("[name='phone']").setValue("8(981)971-57-80");
+        $("[class='checkbox__box']").click();
+        $("[role='button']").click();
+        $(withText("Телефон указан неверно")).shouldBe(visible);
     }
 }
