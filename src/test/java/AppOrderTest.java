022b5f54def8c128931a4bd9a415eac2a2253c44
tokehos <max-lukich@mail.ru>
2021-12-09 15:54:50 +0300

    update tests

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -7,10 +8,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AppOrderTest {
 
+    @BeforeEach
+    public void openBrauser() {
+        open("http://localhost:9999/");
+    }
+
 
     @Test
-    void shouldDebetCardForm() {
-        open("http://localhost:9999/");
+    void shouldDebetCardForm() { // валидные данные
         $("[name='name']").setValue("Лиукконен Максим");
         $("[name='phone']").setValue("+79819715780");
         $("[class='checkbox__box']").click();
@@ -19,22 +24,48 @@ public class AppOrderTest {
     }
 
     @Test
-    void shouldDebetCardFormWithInvalidNameForm() {
-        open("http://localhost:9999/");
+    void shouldDebetCardFormWithInvalidNameForm() { // не валидное ФИО
         $("[name='name']").setValue("Liukkonen Maksim");
         $("[name='phone']").setValue("+79819715780");
         $("[class='checkbox__box']").click();
         $("[role='button']").click();
-        $(withText("Имя и Фамилия указаные неверно")).shouldBe(visible);
+        $(".input_invalid[data-test-id='name']").shouldBe(visible);
     }
 
+//    input_invalid.input__sub
+
     @Test
-    void shouldDebetCardFormWithInvalidPhoneForm() {
-        open("http://localhost:9999/");
+    void shouldDebetCardFormWithInvalidPhoneForm() { // не валидный номер телефона
         $("[name='name']").setValue("Лиукконен Максим");
         $("[name='phone']").setValue("8(981)971-57-80");
         $("[class='checkbox__box']").click();
         $("[role='button']").click();
-        $(withText("Телефон указан неверно")).shouldBe(visible);
+        $(".input_invalid[data-test-id='phone']").shouldBe(visible);
     }
+
+
+    @Test
+    void shouldDebetCardFormWithEmptyForm() { // форма ФИО и номера телефона
+        $("[class='checkbox__box']").click();
+        $("[role='button']").click();
+        $(".input_invalid[data-test-id='name']").shouldBe(visible);
+    }
+
+    @Test
+    void shouldDebetCardFormWithNameFormEmpty() { //  форма номера телефона пустая
+        $("[name='name']").setValue("Лиукконен Максим");
+        $("[class='checkbox__box']").click();
+        $("[role='button']").click();
+        $(".input_invalid[data-test-id='phone']").shouldBe(visible);
+    }
+
+    @Test
+    void shouldDebetCardFormWithCheckBoxEmpty() { // чек бокс пустой
+        $("[name='name']").setValue("Лиукконен Максим");
+        $("[name='phone']").setValue("+79819715780");
+        $("[role='button']").click();
+        $(".input_invalid[data-test-id='agreement']").shouldBe(visible);
+    }
+
+
 }
